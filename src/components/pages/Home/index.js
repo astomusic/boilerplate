20cb8b5469dd83babc957a174b230b8d44f978de
최선진 <astomusic@naver.com>
2019-06-30 13:05:05 +0900

    add counter

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './index.scss';
 
 const Home = () => {
+  const [counter, setCounter] = useState(0);
+  const [startTime, setStartTime] = useState('');
+  const handleCounter = () => {
+    if (counter === 0) {
+      setStartTime(Date.now());
+      setCounter(counter + 1);
+    } else if (counter === 9) {
+      alert(`${(Date.now() - startTime) / 1000}s`);
+      setCounter(0);
+    } else  {
+      setCounter(counter + 1);
+    }
+  }
   return (
     <React.Fragment>
       <div className="home">
         <h1>홈입니다.</h1>
-        <div className="empty">empty</div>
+        <div
+          className="empty"
+          onClick={handleCounter}
+        >
+          {counter ? counter : 'empty'}
+        </div>
       </div>
       
     </React.Fragment>
